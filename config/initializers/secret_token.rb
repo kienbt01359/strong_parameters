5a8d7b1366d69f796a0432b9d9efbe2e3f1d65ee
kienbt <bui.trung.kien@framgia.com>
2013-10-04 09:49:51 +0700

    create Product CRUD with strong parameters

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StrongParameters::Application.config.secret_key_base = 'f2ec682b2c0ffb7fad928a98331c78d4ebb4d2ef52cc375b8c37cae4a506c8b02df80ff09aed07d832fb3f3c07791e903e5dfd40995975c2a5c2165679052886'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StrongParameters::Application.config.session_store :cookie_store, key: '_strong_parameters_session'
